520a572779600a05d97cb2fc617f3244552f25c5
thiagoocordo <thiagoocordo@gmail.com>
2023-05-12 17:26:20 -0300

    .

---
+++ fechas.js
@@ -0,0 +1,6 @@
+hoy = new Date()
+nacimiento = new Date ("Mayo 30,2001")
+tarde = (hoy.getTime()>nacimiento.getTime())
+dia_nac=nacimiento.getDate()
+mes_nac= nacimiento.getMonth()+1
+años_nac=nacimiento.getFullYear()
